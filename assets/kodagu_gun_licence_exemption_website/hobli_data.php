82430d7c5ddd2dfe7fe5c163354be1c1fbba7125
RathanDevaki <rathandevaki@gmail.com>
2021-11-12 16:53:31 +0000

    hobli done..asset

--- assets/kodagu_gun_licence_exemption_website/hobli_data.php
+++ assets/kodagu_gun_licence_exemption_website/hobli_data.php
@@ -16,8 +16,26 @@ if($conn->connect_error)
     die("Connection Error".$conn->connect_error);
     return;
 }
+
+if("GET_TALUK" == $action){
+    $db_data = array();
+    $sql = "SELECT * from Taluk";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            $db_data[] = $row;
+        }
+        // Send back the complete records as a json
+        echo json_encode($db_data);
+    }else{
+        echo "error".$conn -> error;
+    }
+    $conn->close();
+    return;
+}
+
 if("CREATE_TABLE_HOBLI"==$action){
-    $sql="CREATE TABLE IF NOT EXISTS $table(sl_no INT AUTO_INCREMENT UNIQUE KEY not null, hobli_code VARCHAR(10)PRIMARY KEY,hobli_name CHAR(30))";
+    $sql="CREATE TABLE IF NOT EXISTS $table(sl_no INT AUTO_INCREMENT UNIQUE KEY not null, hobli_code VARCHAR(10)PRIMARY KEY,hobli_name CHAR(30),taluk_name varchar(30))";
 
     if($conn->query($sql)===TRUE)
     {
@@ -51,7 +69,8 @@ if("ADD_HOBLI"==$action)
 {
     $hobli_code=$_POST["hobli_code"];
     $hobli_name=$_POST["hobli_name"];
-    $sql="INSERT INTO $table(hobli_code,hobli_name)VALUES('$hobli_code','$hobli_name')";
+    $taluk_name=$_POST["taluk_name"];
+    $sql="INSERT INTO $table(hobli_code,hobli_name,taluk_name)VALUES('$hobli_code','$hobli_name','$taluk_name')";
     $result=$conn->query($sql);
     echo "Success";
     $conn->close();
@@ -61,8 +80,9 @@ if("UPDATE_HOBLI"==$action)
 {
  $hobli_code = $_POST["hobli_code"];
  $hobli_name = $_POST["hobli_name"];
+ $taluk_name = $_POST["taluk_name"];
  $sl_no = $_POST["sl_no"];
- $sql="UPDATE $table SET hobli_code = '$hobli_code', hobli_name='$hobli_name' where sl_no = $sl_no ";
+ $sql="UPDATE $table SET hobli_code = '$hobli_code', hobli_name='$hobli_name',taluk_name='$taluk_name' where sl_no = $sl_no ";
  $result=$conn->query($sql);
     echo "Success";
     $conn->close();
